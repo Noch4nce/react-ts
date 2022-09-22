8cf029ac7f21e8ef1138226ed67de6016bce77c2
Noch4nce <m4rgera123@mail.ru>
2022-09-22 20:48:24 +0500

    user list comp

--- src/components/UserList.tsx
+++ src/components/UserList.tsx
@@ -2,11 +2,20 @@ import React, { FC } from 'react'
 import { IUser } from '../types/types'
 
 interface UserListProps {
-	users: IUser
+	users: IUser[]
 }
 
-const UserList: FC<UserListProps> = () => {
-	return <div></div>
+const UserList: FC<UserListProps> = ({ users }) => {
+	return (
+		<div>
+			{users.map((user) => (
+				<div key={user.id} style={{ padding: 15, border: '1px solid red' }}>
+					{user.id}. {user.name} lives in {user.address.city} on
+					street {user.address.street}
+				</div>
+			))}
+		</div>
+	)
 }
 
 export default UserList
