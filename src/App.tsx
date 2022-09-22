97f22191124943ef47a946c8558ef3e7f7ce1b3a
Noch4nce <m4rgera123@mail.ru>
2022-09-22 20:22:17 +0500

    card

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
 import React from 'react'
+import Card, { CardVariants } from './components/Card'
 
 const App = () => {
-	return <div>Hi</div>
+	return (
+		<div>
+			<Card
+				onClick={() => console.log('click')}
+				variant={CardVariants.primary}
+				width="200px"
+				height="200px"
+			>
+				<button>Button</button>
+			</Card>
+		</div>
+	)
 }
 
 export default App
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,1 +1,41 @@
+import React, { FC, PropsWithChildren } from 'react'
 
+export enum CardVariants {
+	outlined = 'outlined',
+	primary = 'primary'
+}
+
+interface CardProps {
+	width?: string
+	height?: string
+	variant: CardVariants
+	onClick: () => void
+}
+
+const Card: FC<PropsWithChildren<CardProps>> = ({
+	width,
+	height,
+	variant,
+	onClick,
+	children
+}) => {
+	return (
+		<div
+			style={{
+				width,
+				height,
+				border:
+					variant === CardVariants.outlined
+						? '1px solid black'
+						: 'none',
+				backgroundColor:
+					variant === CardVariants.primary ? 'lightgray' : ''
+			}}
+			onClick={onClick}
+		>
+			{children}
+		</div>
+	)
+}
+
+export default Card
