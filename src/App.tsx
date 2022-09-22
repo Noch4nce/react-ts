7912ea9c0ea061314c0acc842f9982b3f5455cfb
Noch4nce <m4rgera123@mail.ru>
2022-09-22 21:38:31 +0500

    fetch users

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,23 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Card, { CardVariants } from './components/Card'
 import UserList from './components/UserList'
 import { IUser } from './types/types'
+import axios from 'axios'
 
 const App = () => {
-	const users: IUser[] = [
-		{
-			id: 1,
-			name: 'Leanne Graham',
-			email: 'Sincere@april.biz',
-			address: {
-				street: 'Kulas Light',
-				city: 'Gwenborough',
-				zipcode: '92998-3874'
-			}
-		},
-		{
-			id: 2,
-			name: 'Ervin Howell',
-			email: 'Shanna@melissa.tv',
-			address: {
-				street: 'Victor Plains',
-				city: 'Wisokyburgh',
-				zipcode: '90566-7771'
-			}
-		}
-	]
+	const [users, setUsers] = useState<IUser[]>([])
+
+	const fetchUsers = async () => {
+		const response = await axios.get<IUser[]>(
+			'https://jsonplaceholder.typicode.com/users'
+		)
+
+		setUsers(response.data)
+	}
+
+	useEffect(() => {
+		fetchUsers()
+	}, [])
 
 	return (
 		<div>
