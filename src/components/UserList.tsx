c258991f922c679c4aea16afe4520622df31d039
Noch4nce <m4rgera123@mail.ru>
2022-09-22 20:30:47 +0500

    user list types

---
+++ src/components/UserList.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react'
+import { IUser } from '../types/types'
+
+interface UserListProps {
+	users: IUser
+}
+
+const UserList: FC<UserListProps> = () => {
+	return <div></div>
+}
+
+export default UserList
