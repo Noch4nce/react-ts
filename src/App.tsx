71e0217c95966074de877eede09b46b8ba3ecf24
Noch4nce <m4rgera123@mail.ru>
2022-09-23 15:16:56 +0500

    routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,32 @@
 import React from 'react'
-import Card, { CardVariants } from './components/Card'
-import Events from './components/Events'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import UserPages from './components/UserPages'
 import TodosPages from './components/TodosPages'
 
 const App = () => {
 	return (
-		<div>
-			<Events />
-			<Card
-				onClick={() => console.log('click')}
-				variant={CardVariants.primary}
-				width="200px"
-				height="200px"
-			>
-				<button>Button</button>
-			</Card>
+		<BrowserRouter>
+			<Routes>
+				<Route path="/users" element={<UserPages />} />
+				<Route path="/todos" element={<TodosPages />} />
+			</Routes>
+		</BrowserRouter>
 
-			<UserPages />
-
-			<TodosPages />
-		</div>
+		// <div>
+		// 	<Events />
+		// 	<Card
+		// 		onClick={() => console.log('click')}
+		// 		variant={CardVariants.primary}
+		// 		width="200px"
+		// 		height="200px"
+		// 	>
+		// 		<button>Button</button>
+		// 	</Card>
+		//
+		// 	<UserPages />
+		//
+		// 	<TodosPages />
+		// </div>
 	)
 }
 
